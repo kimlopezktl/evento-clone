61a397e50adb86a1f0ae659c19a042f16b240754
Kimberly Lopez <klopezktl@gmail.com>
2024-06-05 21:46:15 +0800

    Make the title of the EventsPage dynamic base on the url

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,20 @@
 import H1 from "@/components/H1";
 
-export default function EventsPage() {
+interface EventsPageProps {
+  params: {
+    city: string;
+  };
+}
+export default function EventsPage({ params }: EventsPageProps) {
+  const { city } = params;
+
   return (
     <main className="flex h-auto min-h-[85vh] flex-col items-center px-3 pt-36 text-center">
-      <H1>Events page per city</H1>
+      <H1>
+        {city === "all"
+          ? "All Events"
+          : `Events page in${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
     </main>
   );
 }
